dc736ee44d39ba7c5a06fb3b8d85e7ba22127224
bloomingtonradio <33583367+bloomingtonradio@users.noreply.github.com>
2019-01-07 16:46:38 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,11 +1,14 @@
 var codropsEvents = {
-	'09-22-2017' : '<span>Monroe County Fall Festival<br/>9AM - 2PM, Campbell\'s Park, Ellettesvillle<br/>&nbsp;<br/>Helping Ham\'s Benefit<br/>5:30 PM, MoCo Fairgrounds, Admission $6<br/>5:30: Fox Hunt<br/>6:30 PM: Spaghetti Dinner w/ Texas Toast (Drink $6)<br/>7:30-10:30: Cody Ikerd & Slidewinders (Country Music)<br/><a href="files/hhfb.docx">FLYER</a></span>',
-	'09-23-2017' : '<span>Bloomington Hamfest<br/>8 AM - 4 PM<br/>Monroe County Fairgrounds Community Building<br/>Forums: Raspberry Pi, INARRL, A.R.I.S.S.<br/>Fox Hunt 10:30 AM<br/>VE Testing 1 PM<br/><a href="files/hhfb.docx">FLYER</a></span>',
-	'09-29-2017' : '<span>Hilly Hundred Planning Meeting<br/>7:00 PM, 115 Showers Building</span>',
-	'10-06-2017' : '<span>Hilly Hundred Volunteers Meeting<br/>7:00 PM, Edgewood High School, Elettsville</span>',
-	'10-07-2017' : '<span>2017 Hilly Hundred </span>',
-	'10-08-2017' : '<span>2017 Hilly Hundred </span>',
-	'12-01-2017' : '<span>Annual Christmas Party<br/>Showers Building, Frank McCloskey Room</span>',
-	'12-25-2017' : '<span>Christmas Day</span>',
-	'12-31-2017' : '<span>New Year\'s Eve</span>'
+	'01-04-2019' : '<span>BARC Meeting</span>',
+	'02-02-2019' : '<span>BARC Meeting</span>',
+	'03-01-2019' : '<span>BARC Meeting</span>',
+	'04-05-2019' : '<span>BARC Meeting</span>',
+	'05-03-2019' : '<span>BARC Meeting</span>',
+	'06-07-2019' : '<span>BARC Meeting</span>',
+	'07-05-2019' : '<span>BARC Meeting</span>',
+	'08-02-2019' : '<span>BARC Meeting</span>',
+	'09-06-2019' : '<span>BARC Meeting</span>',
+	'10-04-2019' : '<span>BARC Meeting</span>',
+	'11-02-2019' : '<span>BARC Meeting</span>',
+	'12-06-2019' : '<span>BARC Meeting</span>'
 };
